12e82bfc867066c3cdddbac98c712fb08c52c38d
Vinh <lytranvinh.work@gmail.com>
2024-12-28 12:21:37 +0700

    handle conn sql, set pool

--- internal/initialize/mysql.go
+++ internal/initialize/mysql.go
@@ -1,8 +1,71 @@
 package initialize
 
-import ()
+import (
+	"fmt"
+	"time"
+
+	"github.com/Bot-SomeOne/go-ecommerce/global"
+	"github.com/Bot-SomeOne/go-ecommerce/internal/po"
+	"go.uber.org/zap"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+// Handle err panic
+func checkErrorPanic(err error, errString string) {
+	if err != nil {
+		global.Logger.Error(errString, zap.Error(err))
+		panic(err)
+	}
+}
 
 // Initial my sql
 func InitMysql() {
-	
+	m := global.Config.MySQL
+
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	// dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "%s:%s@tcp(%s:%v)/%s?charset=utf8mb4&parseTime=True&loc=Local"
+	var s = fmt.Sprintf(dsn, m.Username, m.Password, m.Host, m.Port, m.Dbname)
+
+	db, err := gorm.Open(mysql.Open(s), &gorm.Config{
+		// SkipDefaultTransaction: false,
+	})
+
+	checkErrorPanic(err, "MySQL connection failed")
+
+	global.Logger.Info("MySQL connection successful")
+	global.Mdb = db
+
+	// set Pool
+	SetPool()
+
+	// migrate tables
+	MigrateTables()
+}
+
+// InitMysql().SetPool()
+func SetPool() {
+	m := global.Config.MySQL
+
+	sqlDb, err := global.Mdb.DB()
+	if err != nil {
+		fmt.Println("MySQL error: %s::", err)
+		global.Logger.Error("SetPool error", zap.Error(err))
+	}
+
+	sqlDb.SetConnMaxIdleTime(time.Duration(m.MaxIdleConns))    // Thoi gian toi da ket noi nhan doi -> Phuc hoi ket noi
+	sqlDb.SetMaxOpenConns(m.MaxOpenConns)                      // Gioi han so luong ket noi toi da
+	sqlDb.SetConnMaxLifetime(time.Duration(m.ConnMaxLifetime)) // Gioi han thoi gian toi da cua ket noi
+
+}
+
+// migrate tables
+func MigrateTables() {
+	err := global.Mdb.AutoMigrate(
+		&po.User{},
+		&po.Role{},
+	)
+	checkErrorPanic(err, "AutoMigrate tables failed")
+
 }
