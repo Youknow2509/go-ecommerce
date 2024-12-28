f2bfa97786104b551ae1a6e8f8028334d9717dda
Vinh <lytranvinh.work@gmail.com>
2024-12-28 12:21:37 +0700

    handle conn sql, set pool

--- global/global.go
+++ global/global.go
@@ -3,9 +3,11 @@ package global
 import (
 	"github.com/Bot-SomeOne/go-ecommerce/pkg/logger"
 	"github.com/Bot-SomeOne/go-ecommerce/pkg/setting"
+	"gorm.io/gorm"
 )
 
 var (
 	Config setting.Config
 	Logger *logger.LoggerZap
+	Mdb   *gorm.DB
 )
