f7f08f6addda5c229fa87ca6b0d3956e70c2a07e
Vinh <lytranvinh.work@gmail.com>
2024-12-30 17:48:20 +0700

    handle render model and render database sql create

--- internal/wire/user.wire.go
+++ internal/wire/user.wire.go
@@ -1,20 +1,20 @@
 // go:build wireinject
 package wire 
 
-import (
-	"github.com/google/wire"
+// import (
+// 	"github.com/google/wire"
 
-	"github.com/Youknow2509/go-ecommerce/internal/controller"
-	"github.com/Youknow2509/go-ecommerce/internal/repo"
-	"github.com/Youknow2509/go-ecommerce/internal/service"
-)
+// 	"github.com/Youknow2509/go-ecommerce/internal/controller"
+// 	"github.com/Youknow2509/go-ecommerce/internal/repo"
+// 	"github.com/Youknow2509/go-ecommerce/internal/service"
+// )
 
-func InitUserRouterHandle() (*controller.UserController, error) {
-	wire.Build(
-		repo.NewUserRepository,
-		service.NewUserService,
-		controller.NewUserController,
-	)	
+// func InitUserRouterHandle() (*controller.UserController, error) {
+// 	wire.Build(
+// 		repo.NewUserRepository,
+// 		service.NewUserService,
+// 		controller.NewUserController,
+// 	)	
 
-	return new(controller.UserController), nil
-}
+// 	return new(controller.UserController), nil
+// }
