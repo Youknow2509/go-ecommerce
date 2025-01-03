3020127b89288cc674c24828231344fe32869db8
Vinh <lytranvinh.work@gmail.com>
2025-01-03 23:00:40 +0700

    [upgrade]: handle send req to kafka

--- global/global.go
+++ global/global.go
@@ -6,13 +6,15 @@ import (
 	"github.com/Youknow2509/go-ecommerce/pkg/logger"
 	"github.com/Youknow2509/go-ecommerce/pkg/setting"
 	"github.com/redis/go-redis/v9"
+	"github.com/segmentio/kafka-go"
 	"gorm.io/gorm"
 )
 
 var (
-	Config setting.Config
-	Logger *logger.LoggerZap
-	Mdb    *gorm.DB
-	Rdb    *redis.Client
-	Mdbc   *sql.DB
+	Config        setting.Config
+	Logger        *logger.LoggerZap
+	Mdb           *gorm.DB
+	Rdb           *redis.Client
+	Mdbc          *sql.DB
+	KafkaProducer *kafka.Writer
 )
